820e3ada6796a0f982d5deae66461bbddfe11952
Chinmay Ghotkar <chinmayg@vt.edu>
2017-08-13 01:31:57 -0700

    Delete config.py
    Accidently pushed to github

--- config.py
+++
@@ -1,7 +0,0 @@
-# DO NOT PUSH TO GITHUB
-
-#Discord Bot Token
-discord_token = 'MzQ0OTU2NDUxNDQ3NjM1OTY4.DG0X9w.P5ajg2O5E7kmvv7eEuOkr2SgVdg'
-
-#Riot Api Token
-riot_token = 'RGAPI-af1c0e29-8093-4d42-a1da-c5910ea0f87d'
